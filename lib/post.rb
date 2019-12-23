dcba35e4bd629a2c7106904826a3c0c4eecb3d91
Luana Faria Lane <luanasfff@gmail.com>
2019-12-23 22:25:16 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,30 @@
+class Post 
+  attr_accessor :author, :title
+ 
+  @@all = []
+ 
+  def initialize(title)
+    @title = title 
+    
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  
+
+  def author_name
+    if @author == nil 
+      nil 
+    else 
+       @author.name 
+    end 
+end
+
+end 
